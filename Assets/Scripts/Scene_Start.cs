36c017951dc4c0a7518293288af2267d5c9538b8
Your Name <email@example.com>
2019-06-11 21:40:32 +0800

    jamen

---
+++ Assets/Scripts/Scene_Start.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Start : MonoBehaviour
+{
+
+    float _timer;
+    GameObject anyKeyObj;
+
+    // Use this for initialization
+    void Start()
+    {
+        _timer = 0;
+        anyKeyObj = GameObject.Find("Text");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer % 0.5f > 0.25f)
+        {
+            anyKeyObj.SetActive(true);
+        }
+        else
+        {
+            anyKeyObj.SetActive(false);
+        }
+
+        if (_timer > 100 || Input.anyKeyDown)
+        {
+            //任意键按下，则直接跳转主菜单场景
+            GoToMainMenu();
+        }
+    }
+
+    void GoToMainMenu()
+    {
+        SceneManager.LoadScene("Mainmenu");
+    }
+}
