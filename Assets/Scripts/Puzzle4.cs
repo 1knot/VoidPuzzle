dbb56e926645d9ae81255fefd25e944d5967f60e
1knot <1037329504@qq.com>
2019-06-13 16:06:42 +0800

    新增VirtualPuzzle4类用于计算求解

--- Assets/Scripts/Puzzle4.cs
+++ Assets/Scripts/Puzzle4.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿//using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class Puzzle4
 {
@@ -8,7 +10,9 @@ public class Puzzle4
     Dictionary<string, RectTransform> curUnitRT = new Dictionary<string, RectTransform>();
     List<int> data = new List<int>();
     private int spacePos = 0;
+
     public Puzzle4(long seed){
+
         for(int i = 1; i <= 16; i++){
             string id = i.ToString();
             RectTransform idRT =  GameObject.Find(id).GetComponent<RectTransform>();
@@ -38,6 +42,76 @@ public class Puzzle4
             string unitId = data[i - 1].ToString();
             curUnitRT[unitId].anchoredPosition = unitPos[posId];
         }
+        string a = "";
+        for(int i = 0; i<16; i++){
+            a += data[i] +" ";
+        }
+        Debug.Log(a);
+    }
+    private void rand(int step){
+        if(step<=0){
+            return;
+        }
+        Debug.Log(step);
+
+        int pre = 0;
+
+        for(int i=0; i<step; i++){
+            int r = Random.Range(0, 4);
+            if(r + pre == 3){
+                continue;
+            }
+            switch(r){
+                case 0://上
+                    if(this.canUp()){
+                        this.goUp();
+                        pre = 0;
+                    }
+                    break;
+                case 1://左
+                    if(this.canLeft()){
+                        this.goLeft();
+                        pre = 1;
+                    }
+                    break;
+                case 2://右
+                    if(this.canRight()){
+                        this.goRight();
+                        pre = 2;
+                    }
+                    break;
+                case 3://下
+                    if(this.canDown()){
+                        this.goDown();
+                        pre = 3;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+            
+    }
+    public Puzzle4(){
+        for(int i = 1; i <= 16; i++){
+            string id = i.ToString();
+            RectTransform idRT =  GameObject.Find(id).GetComponent<RectTransform>();
+            curUnitRT.Add(id, idRT);
+            unitPos.Add(id, idRT.anchoredPosition);
+            data.Add(i);
+        }
+        this.spacePos = 16;
+        this.rand(100);
+
+    }
+    public Puzzle4(Puzzle4 p){
+        for(int i = 1; i <= 16; i++){
+            string id = i.ToString();
+            this.unitPos[id] = p.unitPos[id];
+            this.curUnitRT[id] = p.curUnitRT[id];
+            this.data[i - 1] = p.data[i - 1];
+        }
+        this.spacePos = p.spacePos;
     }
     //获取当前地图种子
     public long getSeed(){
@@ -138,5 +212,63 @@ public class Puzzle4
         }else{
             return false;
         }
+    }
+    public static bool canSlove(long seed){
+        List<int> pz = new List<int>();
+        List<int> v = new List<int>();
+        int zeroPos = 0;
+        long weight = 1;
+        for(int i = 1; i <= 16; i++){
+            v.Add(i);
+            weight *= i;
+        }
+        for(int i = 16; i > 0; i--){
+            weight /= i;
+            int p = (int)(seed / weight);
+            seed = seed % weight;
+            pz.Add(v[p]);
+            if(v[p] == 16){
+                zeroPos = pz.Count;
+            }
+            v.RemoveAt(p);
+        }
+        if(zeroPos != 16){
+            if(zeroPos % 4 != 0){
+                while((zeroPos + 1) % 4 != 1){
+                    pz[zeroPos - 1] = pz[zeroPos - 1 + 1];
+                    zeroPos++;
+                }
+            }
+            while(zeroPos != 16){
+                pz[zeroPos - 1] = pz[zeroPos - 1 + 4];
+                zeroPos += 4;
+            }
+            pz[zeroPos - 1] = 16;
+        }
+        int ans = 0;
+        for(int i = 1; i < 16; i++){
+            for(int j = 0; j < i; j++){
+                if(pz[i] < pz[j]){
+                    ans++;
+                }
+            }
+        }
+        if(ans % 2 == 1){
+            return false;
+        }else{
+            return true;
+        }
+        
+    }
+    
+    public void slove(){
+        VirtualPuzzle4 v = new VirtualPuzzle4(this.data);
+        v.slove();
+        string a = "";
+        for(int i = 0; i < v.step; i++){
+            a += " " + v.path[i];
+        }
+        a = "step: " + v.step + "  " + a;
+        Debug.Log(a);
     }
 }
---
+++ Assets/Scripts/VirtualPuzzle4.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class VirtualPuzzle4
+{
+    public List<int> data = new List<int>();
+    public int spacePos = 0;
+    private int limit;
+    
+    private bool isFinish = false;
+    public int step = 0;
+    public char[] path = new char[500];
+    public VirtualPuzzle4(List<int> pdata){
+        for(int i = 1; i <= 16; i++){
+            this.data.Add(pdata[i - 1]);
+            if(pdata[i - 1] == 16){
+                this.spacePos = i;
+            }
+        }
+        this.limit = this.getDistance();
+        if(this.limit == 0){
+            this.isFinish = true;
+        }
+    }
+    private int getDistance(){
+        int distance = 0;
+        for(int i = 1; i <= 16; i++){
+            bool flag = true;
+            for(int j = 1; j <= this.data.Count && flag; j++){
+                if(i == this.data[j - 1]){
+                    distance += Math.Abs(i - j) / 4 + Math.Abs(((i - 1) % 4) - ((j - 1) % 4));
+                    flag = false;
+                }
+            }
+        }
+        return distance;
+    }
+    private void swap(int pos){
+        this.data[this.spacePos - 1] = this.data[pos - 1];
+        this.data[pos - 1] = 16;
+        this.spacePos = pos;
+    }
+
+    private void dfs(int deep, int pre){
+        if(this.isFinish){
+            return;
+        }
+        int cost = getDistance();
+        if(deep == limit){
+            if(cost == 0){
+                isFinish = true;
+                step = deep;
+            }else{
+                return;
+            }
+        }else if(deep < limit){
+            if(cost == 0){
+                isFinish = true;
+                step = deep;
+                return;
+            }
+        }
+        
+        for(int i = 0; i <= 4; i++){
+            if(i + pre == 3 && deep>0)
+                continue;
+            switch(i){
+                case 0://上
+                    if(this.canUp()){
+                        this.swap(this.spacePos - 4);
+                        int p = this.getDistance();
+                        if(p + deep < this.limit && !isFinish){
+                            path[deep] = 'D';
+                            dfs(deep + 1, i);
+                        }
+                        this.swap(this.spacePos + 4);
+                    }
+                    break;
+                case 1://左
+                    if(this.canLeft()){
+                        this.swap(this.spacePos - 1);
+                        int p = this.getDistance();
+                        if(p + deep < this.limit && !isFinish){
+                            path[deep] = 'R';
+                            dfs(deep + 1, i);
+                        }
+                        this.swap(this.spacePos + 1);
+                    }
+                    break;
+                case 2://右
+                    if(this.canRight()){
+                        this.swap(this.spacePos + 1);
+                        int p = this.getDistance();
+                        if(p + deep < this.limit && !isFinish){
+                            path[deep] = 'L';
+                            dfs(deep + 1, i);
+                        }
+                        this.swap(this.spacePos - 1);
+                    }
+                    break;
+                case 3://下
+                    if(this.canDown()){
+                        this.swap(this.spacePos + 4);
+                        int p = this.getDistance();
+                        if(p + deep < this.limit && !isFinish){
+                            path[deep] = 'U';
+                            dfs(deep + 1, i);
+                        }
+                        this.swap(this.spacePos - 4);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void slove(){
+        while(!this.isFinish && this.step<=50){
+            dfs(0, 0);
+            if(!isFinish)
+                limit++;
+        }
+    }
+
+    private bool canUp(){
+        if (spacePos - 4 >= 1){
+            return true;   
+        }else{
+            return false;
+        }
+    }
+    private bool canDown(){
+        if(spacePos + 4 <= 16){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    private bool canLeft(){
+        if((spacePos - 1) % 4 != 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    private bool canRight(){
+        if(spacePos % 4 != 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    /* 
+    public bool isFinish(){
+        for(int i = 0; i < 16; i++){
+            if(this.data[i] != i+1)
+                return false;
+        }
+        return true;
+    }
+    */
+}
